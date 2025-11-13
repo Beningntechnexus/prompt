4008c101fdc3f5b4968a8a8212b6b795a1a1b5da
Zike Benign <zikebenign@gmail.com>
2025-11-13 11:18:00 +0000

    in .idx change the icon.png to my own icond  BBprompts

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -3,7 +3,7 @@ export default function Favicon() {
   return (
     <link
       rel="icon"
-      href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='%2387ceeb' d='M50 10 C 20 20, 20 80, 50 90 C 80 80, 80 20, 50 10 Z M 50 20 Q 40 30, 50 40 Q 60 30, 50 20 Z M 40 45 Q 30 55, 40 65 M 60 45 Q 70 55, 60 65 M 50 70 Q 45 80, 50 85 Q 55 80, 50 70' /><path fill='white' d='M50,30 C60,30 65,40 65,50 C65,65 50,75 50,75 C50,75 35,65 35,50 C35,40 40,30 50,30 Z M50 35 C 45 35 40 40 40 50 C 40 60 50 70 50 70 C 50 70 60 60 60 50 C 60 40 55 35 50 35 Z'/><animateTransform attributeName='transform' type='translate' values='0 0; 0 -2; 0 0' dur='1.5s' repeatCount='indefinite'/></svg>"
+      href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' rx='20' fill='%2387ceeb'/><text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' font-size='60' font-family='Arial, sans-serif' font-weight='bold' fill='white'>BB</text></svg>"
       type="image/svg+xml"
     />
   );
