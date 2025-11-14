3e910ee56a27ece6ed4d3dc83d377fc80bcd0bf6
Zike Benign <zikebenign@gmail.com>
2025-11-14 08:43:35 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/icon/route.tsx
+++ src/app/icon/route.tsx
@@ -13,28 +13,23 @@ export const contentType = 'image/png';
 // Image generation
 export function GET() {
   return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 24,
-          background: '#87ceeb',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: 'white',
-          borderRadius: '6px',
-          fontWeight: 'bold',
-        }}
-      >
-        BB
-      </div>
-    ),
-    // ImageResponse options
+    <div
+      style={{
+        fontSize: 24,
+        background: '#87ceeb',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        borderRadius: '6px',
+        fontWeight: 'bold',
+      }}
+    >
+      BB
+    </div>,
     {
-      // For convenience, we can re-use the exported icons config
       ...size,
     }
   );
